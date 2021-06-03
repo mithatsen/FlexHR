2567d535efb13203df7a9bde07ccbe17e7165bfb
mithatsen <mithatsen45@gmail.com>
2021-06-03 10:00:17 +0300

    View Components added

---
+++ FlexHR/FlexHR.Web/ViewComponents/StaffPayment.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlexHR.Web.ViewComponents
+{
+    public class StaffPayment : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ FlexHR/FlexHR.Web/ViewComponents/StaffShift.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlexHR.Web.ViewComponents
+{
+    public class StaffShift : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
