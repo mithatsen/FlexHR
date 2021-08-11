597d0d837e49ff37a7885ae1d69163fa006e3f22
mithatsen <mithatsen45@gmail.com>
2021-08-11 11:32:21 +0300

    Roles Added To Db And Add Staff withMULTİPLE SELECT ROLES

--- FlexHR/FlexHR.DTO/Dtos/RoleDtos/ListRoleDto.cs
+++ FlexHR/FlexHR.DTO/Dtos/RoleDtos/ListRoleDto.cs
@@ -9,6 +9,8 @@ namespace FlexHR.DTO.Dtos.RoleDtos
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int AuthorizeTypeGeneralSubTypeId { get; set; }
+        public string AuthorizeType { get; set; }
         public bool IsActive { get; set; }
     }
 }
--- FlexHR/FlexHR.Web/Controllers/StaffController.cs
+++ FlexHR/FlexHR.Web/Controllers/StaffController.cs
@@ -10,6 +10,7 @@ using FlexHR.DTO.Dtos.StaffShiftDtos;
 using FlexHR.Entity.Concrete;
 using FlexHR.Entity.Enums;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
@@ -23,28 +24,31 @@ namespace FlexHR.Web.Controllers
     public class StaffController : Controller
     {
         private readonly IStaffService _staffService;
-        
+
         private readonly IMapper _mapper;
-     //   private readonly IStaffRoleService _staffRoleService;
+        //   private readonly IStaffRoleService _staffRoleService;
         private readonly IGeneralSubTypeService _generalSubTypeService;
         private readonly IAppRoleService _appRoleService;
         private readonly IStaffPersonelInfoService _staffPersonelInfoService;
         private readonly IStaffOtherInfoService _staffOtherInfoService;
         private readonly IStaffFileService _staffFileService;
+        protected readonly UserManager<AppUser> _userManager;
+        protected readonly RoleManager<AppRole> _roleManager;
 
-        public StaffController(IStaffService staffService, IMapper mapper, IGeneralSubTypeService generalSubTypeService,IStaffPersonelInfoService staffPersonelInfoService,
-                                      IAppRoleService appRoleService,IStaffOtherInfoService staffOtherInfoService, IStaffFileService staffFileService )
+        public StaffController(IStaffService staffService, IMapper mapper, IGeneralSubTypeService generalSubTypeService, IStaffPersonelInfoService staffPersonelInfoService,
+                                      IAppRoleService appRoleService, IStaffOtherInfoService staffOtherInfoService, IStaffFileService staffFileService, UserManager<AppUser> userManager,
+                                       RoleManager<AppRole> roleManager)
         {
             _staffService = staffService;
             _mapper = mapper;
-      
+            _userManager = userManager;
             //_staffRoleService = staffRoleService;
             _generalSubTypeService = generalSubTypeService;
             _appRoleService = appRoleService;
             _staffPersonelInfoService = staffPersonelInfoService;
             _staffOtherInfoService = staffOtherInfoService;
             _staffFileService = staffFileService;
-
+            _roleManager = roleManager;
 
         }
 
@@ -53,7 +57,8 @@ namespace FlexHR.Web.Controllers
             var result = _staffService.Get(x => x.IsActive == true, null, "StaffFile");
 
             ViewBag.ContractTypes = new SelectList(_generalSubTypeService.GetGeneralSubTypeByGeneralTypeId((int)GeneralTypeEnum.ContractType), "GeneralSubTypeId", "Description");
-            ViewBag.Roles = new SelectList(_appRoleService.GetAll(), "Id", "Name");
+            ViewBag.Roles = new SelectList(_appRoleService.Get(x => x.AuthorizeTypeGeneralSubTypeId == 125), "Id", "Description");
+            ViewBag.PageRoles = new SelectList(_appRoleService.Get(x => x.AuthorizeTypeGeneralSubTypeId == 126), "Id", "Description");
 
 
             var models = _mapper.Map<List<ListStaffDto>>(result);
@@ -69,15 +74,15 @@ namespace FlexHR.Web.Controllers
         }
 
         public IActionResult StaffSearchWithAjax(IFormCollection formData)
-          {
+        {
             IEnumerable<Staff> result;
-            if (formData==null || String.IsNullOrWhiteSpace(formData.Keys.FirstOrDefault()) )
+            if (formData == null || String.IsNullOrWhiteSpace(formData.Keys.FirstOrDefault()))
             {
-                 result = _staffService.GetAll();
+                result = _staffService.GetAll();
             }
             else
             {
-                 result = _staffService.GetStaffBySearchString(formData.Keys.FirstOrDefault());
+                result = _staffService.GetStaffBySearchString(formData.Keys.FirstOrDefault());
             }
             var models = _mapper.Map<List<ListStaffDto>>(result);
             foreach (var item in models)
@@ -86,48 +91,51 @@ namespace FlexHR.Web.Controllers
 
                 item.PictureUrl = picture != null ? picture.FileFullPath + picture.FileName : null;
             }
-            return PartialView("_GetStaffCardBySearch",models);
+            return PartialView("_GetStaffCardBySearch", models);
         }
         [HttpPost]
-        public IActionResult AddStaffWithAjax(AddStaffDto modal)
+        public async Task<IActionResult> AddStaffWithAjax(AddStaffDto modal)
         {
+            var appUser = await _userManager.FindByNameAsync(modal.UserName);
+            if (appUser != null)
+            {
+                return Json("false");
+            }
             var staffResult = new Staff();
             if (modal.WillUseSystem == false)
             {
-                staffResult = _staffService.AddResult(new Staff
-                {
-                    NameSurname = modal.NameSurname,
-                    JobJoinDate = modal.JobJoinDate,
-                    IsActive = true,
-                    WillUseSystem = modal.WillUseSystem,
-                    EmailJob = modal.EmailJob,
-                    EmailPersonal = modal.EmailPersonal,
-                    JobFinishDate = modal.JobFinishDate,
-                    PhoneJob = modal.PhoneJob,
-                    PhonePersonal = modal.PhonePersonal,
-                });
+                staffResult = _staffService.AddResult(_mapper.Map<Staff>(modal));
             }
             else
             {
-                staffResult = _staffService.AddResult(new Staff
+                staffResult = _staffService.AddResult(_mapper.Map<Staff>(modal));
+                AppUser user = new AppUser()
                 {
-                    NameSurname = modal.NameSurname,
-                    JobJoinDate = modal.JobJoinDate,
-                    IsActive = true,
-                  //  UserName = modal.UserName,
-                  //  Password = modal.Password,
-                    WillUseSystem = modal.WillUseSystem,
-                    EmailJob = modal.EmailJob,
-                    EmailPersonal = modal.EmailPersonal,
-                    JobFinishDate = modal.JobFinishDate,
-                    PhoneJob = modal.PhoneJob,
-                    PhonePersonal = modal.PhonePersonal,
-                });
+                    UserName = modal.UserName,
+                    Email = modal.EmailPersonal,
+                    StaffId = staffResult.StaffId,
+                    IsActive = true
+                };
+
+                await _userManager.CreateAsync(user, modal.Password);              
+                var role = _appRoleService.Get(x => x.Id == modal.RoleId).FirstOrDefault().Name;
+
+                var dene = await _userManager.AddToRoleAsync(user, role);
+
+                foreach (var item in modal.PageRoles)
+                {
+                    var rolePage = _appRoleService.Get(x => x.Id == item).FirstOrDefault().Name;
+                    var isInRole = await _userManager.IsInRoleAsync(user, rolePage);
+                    if (!isInRole)
+                    {
+                        await _userManager.AddToRoleAsync(user, rolePage);
+                    }
+                    await _userManager.AddToRoleAsync(user, rolePage);
+                }
             }
 
             var staffId = staffResult.StaffId;
-           
-          //  _staffRoleService.Add(new StaffRole { StaffId = staffId, RoleId = modal.RoleId });
+
             _staffOtherInfoService.Add(new StaffOtherInfo { StaffId = staffId, IsActive = true });
             _staffPersonelInfoService.Add(new StaffPersonelInfo { StaffId = staffId, IsActive = true });
 
