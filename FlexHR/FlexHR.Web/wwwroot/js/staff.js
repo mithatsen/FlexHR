07a5b069dccc11a0cdf528c275e8cbde5ed2aad4
mithatsen <mithatsen45@gmail.com>
2021-08-25 10:21:18 +0300

    validation fixed

--- FlexHR/FlexHR.Web/wwwroot/js/staff.js
+++ FlexHR/FlexHR.Web/wwwroot/js/staff.js
@@ -2,7 +2,7 @@
 //Modal Add Staff 
 
 // Create a FormValidation instance
-var fvAddStaff = FormValidation.formValidation(document.getElementById('addStaffFormModal29'),
+var fvAddStaffWithoutUser = FormValidation.formValidation(document.getElementById('addStaffFormModal29'),
     {
         fields: {
             NameSurname: {
@@ -20,7 +20,7 @@ var fvAddStaff = FormValidation.formValidation(document.getElementById('addStaff
                     notEmpty: {
                         message: 'E-posta alanı boş geçilemez'
                     }
-                    
+
                 }
             },
             EmailJob: {
@@ -77,57 +77,287 @@ var fvAddStaff = FormValidation.formValidation(document.getElementById('addStaff
         }
     });
 
-function staffAddClickFunction() {
-    debugger;
-    fvAddStaff.validate().then(function (status) {
-        // Update the login button content based on the validation status
-        if (status == 'Valid') {
-            console.log($("#ContractTypeSelect").val());
-            var formData = {
-                NameSurname: $("#NameSurname").val(),
-                EmailJob: $("#EmailJob").val(),
-                PhoneJob: $("#PhoneJob").val(),
-                EmailPersonal: $("#EmailPersonal").val(),
-                PhonePersonal: $("#PhonePersonal").val(),
-                ContractTypeGeneralSubTypeId: $("#ContractTypeSelect").val(),
-                JobFinishDate: $("#JobFinishDate").val(),
-                JobJoinDate: $("#JobJoinDate").val(),
-                UserName: $("#UserName").val(),
-                WillUseSystem: $("#WillUseSystem")[0].checked,
-                RoleId: $("#RoleId").val(),
-                Password: $("#Password").val(),
-                PageRoles: $("#pageRoles").val(),
+var fvAddStaffWithUser;
 
-            };
-            $.ajax({
-                method: 'post',
-                url: '/Staff/AddStaffWithAjax',
-                data: formData,
-                success: function (data) {
-                    if (data == "true") {
-                        Swal.fire({
-                            title: "Eklendi!",
-                            text: "Kaydınız eklendi.",
-                            icon: "success",
-                        }).then(function () {
-                            window.location.reload()
-                        })
-                    } else {
+function ValidateStaff(status) {
 
-                        Swal.fire({
-                            title: "Başarısız!",
-                            text: "Kullanıcı adı zaten var.",
-                            icon: "error",
-                        }).then(function () {
-                            window.location.reload()
-                        })
+    if (status == "true" && fvAddStaffWithoutUser != null) {
+        fvAddStaffWithoutUser.destroy();
+        fvAddStaffWithUser = FormValidation.formValidation(document.getElementById('addStaffFormModal29'),
+            {
+                fields: {
+                    NameSurname: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Ad soyad alanı boş geçilemez'
+                            }
+                        }
+                    },
+                    EmailPersonal: {
+                        validators: {
+                            emailAddress: {
+                                message: 'Geçerli bir E-posta adresi girin'
+                            },
+                            notEmpty: {
+                                message: 'E-posta alanı boş geçilemez'
+                            }
+
+                        }
+                    },
+                    EmailJob: {
+                        validators: {
+
+                            emailAddress: {
+                                message: 'Geçerli bir E-posta adresi girin'
+                            }
+                        }
+                    },
+                    PhonePersonal: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Telefon alanı boş geçilemez'
+                            },
+                            phone: {
+                                country: 'US',
+                                message: 'Geçerli bir numara girin'
+                            }
+                        }
+                    },
+                    PhoneJob: {
+                        validators: {
+                            phone: {
+                                country: 'US',
+                                message: 'Geçerli bir numara girin'
+                            }
+                        }
+                    },
+                    JobJoinDate: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Tarih alanı boş geçilemez'
+                            },
+
+                        }
+                    },
+                    ContractTypeId: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Sözleşme türü seçiniz'
+                            },
+
+                        }
+                    },
+                    UserNameAddStaff: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Kullanıcı adı boş geçilemez'
+                            },
+
+                        }
+                    },
+                    PasswordAddStaff: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Şifre boş geçilemez'
+                            },
+
+                        }
                     }
 
-                }
 
-            })
-        }
-    });
+                },
+                plugins: {
+                    trigger: new FormValidation.plugins.Trigger(),
+                    // Bootstrap Framework Integration
+                    bootstrap: new FormValidation.plugins.Bootstrap(),
+
+                }
+            });
+    }
+    else {
+        fvAddStaffWithUser.destroy();
+        fvAddStaffWithoutUser = FormValidation.formValidation(document.getElementById('addStaffFormModal29'),
+            {
+                fields: {
+                    NameSurname: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Ad soyad alanı boş geçilemez'
+                            }
+                        }
+                    },
+                    EmailPersonal: {
+                        validators: {
+                            emailAddress: {
+                                message: 'Geçerli bir E-posta adresi girin'
+                            },
+                            notEmpty: {
+                                message: 'E-posta alanı boş geçilemez'
+                            }
+
+                        }
+                    },
+                    EmailJob: {
+                        validators: {
+
+                            emailAddress: {
+                                message: 'Geçerli bir E-posta adresi girin'
+                            }
+                        }
+                    },
+                    PhonePersonal: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Telefon alanı boş geçilemez'
+                            },
+                            phone: {
+                                country: 'US',
+                                message: 'Geçerli bir numara girin'
+                            }
+                        }
+                    },
+                    PhoneJob: {
+                        validators: {
+                            phone: {
+                                country: 'US',
+                                message: 'Geçerli bir numara girin'
+                            }
+                        }
+                    },
+                    JobJoinDate: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Tarih alanı boş geçilemez'
+                            },
+
+                        }
+                    },
+                    ContractTypeId: {
+                        validators: {
+                            notEmpty: {
+                                message: 'Sözleşme türü seçiniz'
+                            },
+
+                        }
+                    }
+
+
+                },
+                plugins: {
+                    trigger: new FormValidation.plugins.Trigger(),
+                    // Bootstrap Framework Integration
+                    bootstrap: new FormValidation.plugins.Bootstrap(),
+
+                }
+            });
+    }
+
+}
+
+
+function staffAddClickFunction() {
+
+    if ($("#WillUseSystem")[0].checked == true) {
+        fvAddStaffWithUser.validate().then(function (status) {
+            if (status == 'Valid') {
+
+                var formData = {
+                    NameSurname: $("#NameSurname").val(),
+                    EmailJob: $("#EmailJob").val(),
+                    PhoneJob: $("#PhoneJob").val(),
+                    EmailPersonal: $("#EmailPersonal").val(),
+                    PhonePersonal: $("#PhonePersonal").val(),
+                    ContractTypeGeneralSubTypeId: $("#ContractTypeSelect").val(),
+                    JobFinishDate: $("#JobFinishDate").val(),
+                    JobJoinDate: $("#JobJoinDate").val(),
+                    UserName: $("#UserName").val(),
+                    WillUseSystem: $("#WillUseSystem")[0].checked,
+                    RoleId: $("#RoleId").val(),
+                    Password: $("#Password").val(),
+                    PageRoles: $("#pageRoles").val(),
+
+                };
+                $.ajax({
+                    method: 'post',
+                    url: '/Staff/AddStaffWithAjax',
+                    data: formData,
+                    success: function (data) {
+                        if (data == "true") {
+                            Swal.fire({
+                                title: "Eklendi!",
+                                text: "Kaydınız eklendi.",
+                                icon: "success",
+                            }).then(function () {
+                                window.location.reload()
+                            })
+                        } else {
+
+                            Swal.fire({
+                                title: "Başarısız!",
+                                text: "Kullanıcı adı zaten var.",
+                                icon: "error",
+                            }).then(function () {
+                                window.location.reload()
+                            })
+                        }
+
+                    }
+
+                })
+            }
+        });
+    }
+    else {
+        fvAddStaffWithoutUser.validate().then(function (status) {
+            if (status == 'Valid') {
+
+                var formData = {
+                    NameSurname: $("#NameSurname").val(),
+                    EmailJob: $("#EmailJob").val(),
+                    PhoneJob: $("#PhoneJob").val(),
+                    EmailPersonal: $("#EmailPersonal").val(),
+                    PhonePersonal: $("#PhonePersonal").val(),
+                    ContractTypeGeneralSubTypeId: $("#ContractTypeSelect").val(),
+                    JobFinishDate: $("#JobFinishDate").val(),
+                    JobJoinDate: $("#JobJoinDate").val(),
+                    UserName: $("#UserName").val(),
+                    WillUseSystem: $("#WillUseSystem")[0].checked,
+                    RoleId: $("#RoleId").val(),
+                    Password: $("#Password").val(),
+                    PageRoles: $("#pageRoles").val(),
+
+                };
+                $.ajax({
+                    method: 'post',
+                    url: '/Staff/AddStaffWithAjax',
+                    data: formData,
+                    success: function (data) {
+                        if (data == "true") {
+                            Swal.fire({
+                                title: "Eklendi!",
+                                text: "Kaydınız eklendi.",
+                                icon: "success",
+                            }).then(function () {
+                                window.location.reload()
+                            })
+                        } else {
+
+                            Swal.fire({
+                                title: "Başarısız!",
+                                text: "Kullanıcı adı zaten var.",
+                                icon: "error",
+                            }).then(function () {
+                                window.location.reload()
+                            })
+                        }
+
+                    }
+
+                })
+            }
+        });
+    }
+  
 }
 
 /*sözleşme türü süreli ise tarih seçme alanı disabled olmamalı, süresizse disable olmalı */
@@ -152,12 +382,17 @@ $('#WillUseSystem').on('change', function () {
         $("#dvUserName").attr("hidden", false);
         $("#dvRoles").attr("hidden", false);
         $("#dvAccessType").attr("hidden", false);
+
+        ValidateStaff("true");
+
+
     }
     else {
         $("#dvPassword").attr("hidden", true);
         $("#dvUserName").attr("hidden", true);
         $("#dvRoles").attr("hidden", true);
         $("#dvAccessType").attr("hidden", true);
+        ValidateStaff("false");
     }
 })
 $('#pageRoles').select2({
