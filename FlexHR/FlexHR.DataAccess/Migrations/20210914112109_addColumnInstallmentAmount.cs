5aeee9cc5dbfb129ac39f4d659a005c20c8e77d3
DESKTOP-GRI9IBK\korci <korcilmuhammet@gmail.com>
2021-09-14 15:55:27 +0300

    amount instalment migration

---
+++ FlexHR/FlexHR.DataAccess/Migrations/20210914112109_addColumnInstallmentAmount.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlexHR.DataAccess.Migrations
+{
+    public partial class addColumnInstallmentAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "InstallmentAmount",
+                table: "TakePayment",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstallmentAmount",
+                table: "TakePayment");
+        }
+    }
+}
